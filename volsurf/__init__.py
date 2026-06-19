08c36f065a87686aa5a81eac46c66ed74172d994
superkush06 <superkush06@gmail.com>
2026-06-19 10:20:26 +0530

    feat: flat public API at the volsurf package root

--- volsurf/__init__.py
+++ volsurf/__init__.py
@@ -1,3 +1,17 @@
-"""vol-surface."""
+"""vol-surface: implied volatility surface calibration."""
+
+from .black_scholes import BlackScholes
+from .iv import IVSolverError, implied_vol
+from .noarb import butterfly_violations, calendar_violations, total_variance
+from .sabr import SABRParams, sabr_iv
+from .sabr_fit import fit_sabr
+from .svi import SVIRawParams, svi_iv, svi_w
 
 __version__ = "0.1.0"
+__all__ = [
+    "BlackScholes", "implied_vol", "IVSolverError",
+    "SABRParams", "sabr_iv", "fit_sabr",
+    "SVIRawParams", "svi_iv", "svi_w",
+    "butterfly_violations", "calendar_violations", "total_variance",
+    "__version__",
+]
